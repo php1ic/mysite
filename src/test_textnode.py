9cec1b3b2c42d18a942b9d5603a8c5d8687c4265
Ian Cullen <ian.james.cullen@gmail.com>
2024-09-07 18:35:34 +0100

    Add split node function and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -33,5 +33,40 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.value, "This is bold")
 
 
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_example(self):
+        node = textnode.TextNode("This is text with a `code block` word", textnode.text_type_text)
+        new_nodes = textnode.split_nodes_delimiter([node], "`", textnode.text_type_code)
+
+        nodes = [
+            textnode.TextNode("This is text with a ", textnode.text_type_text),
+            textnode.TextNode("code block", textnode.text_type_code),
+            textnode.TextNode(" word", textnode.text_type_text),
+        ]
+        self.assertEqual(new_nodes, nodes)
+
+    def test_bold(self):
+        node = textnode.TextNode("This is text with a **bolded** word", textnode.text_type_text)
+        new_nodes = textnode.split_nodes_delimiter([node], "**", textnode.text_type_bold)
+
+        nodes = [
+            textnode.TextNode("This is text with a ", textnode.text_type_text),
+            textnode.TextNode("bolded", textnode.text_type_bold),
+            textnode.TextNode(" word", textnode.text_type_text),
+        ]
+        self.assertEqual(new_nodes, nodes)
+
+    def test_italic(self):
+        node = textnode.TextNode("This is text with a *italic* word", textnode.text_type_text)
+        new_nodes = textnode.split_nodes_delimiter([node], "*", textnode.text_type_italic)
+
+        nodes = [
+            textnode.TextNode("This is text with a ", textnode.text_type_text),
+            textnode.TextNode("italic", textnode.text_type_italic),
+            textnode.TextNode(" word", textnode.text_type_text),
+        ]
+        self.assertEqual(new_nodes, nodes)
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -40,3 +40,29 @@ def text_node_to_html_node(text_node: TextNode):
     if text_node.text_type == text_type_image:
         return leafnode.LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"Unknown text type: {text_node.text_type}")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = list()
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+
+        sub_nodes = list()
+        subs = node.text.split(delimiter)
+
+        if len(subs) % 2 == 0:
+            raise ValueError(f"ERROR: Unmatched {delimiter}")
+
+        for sub in range(len(subs)):
+            if subs[sub] == "":
+                continue
+
+            if sub % 2 == 0:
+                sub_nodes.append(TextNode(subs[sub], text_type_text))
+            else:
+                sub_nodes.append(TextNode(subs[sub], text_type))
+
+        new_nodes.extend(sub_nodes)
+
+    return new_nodes
