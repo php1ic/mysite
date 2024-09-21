d5b5051aa6998cfe0fad75801ff95c421e7530d3
Ian Cullen <ian.james.cullen@gmail.com>
2024-09-21 18:42:05 +0100

    Copying of files and cleanup is now working

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
-import textnode
+import os
+import shutil
+
+
+def copy_fn(source, destination):
+    if os.path.exists(destination):
+        print(f"The destination directory <{destination}> already exists, deleting")
+        shutil.rmtree(destination)
+
+    def verbose_copy(src, dst):
+        print(f"Copying {src} -> {dst}")
+        shutil.copy2(src, dst)
+
+    shutil.copytree(source, destination, copy_function=verbose_copy)
 
 
 def main():
-    tn = textnode.TextNode("Some text", "italic", "https://boot.dev")
-    print(tn)
+    copy_fn("static", "public")
 
 
 if __name__ == "__main__":
